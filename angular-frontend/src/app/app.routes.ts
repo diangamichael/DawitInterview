3857b4f54ac78a5ceed98effd8a7b6ca68297fc1
Dianga Michael <diangamichael@gmail.com>
2024-02-26 20:43:42 +0300

    Creating routes deleting unneded files

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './Pages/homepage/homepage.component';
+import { AboutpageComponent } from './Pages/aboutpage/aboutpage.component';
+import { ContactComponent } from './Pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:HomepageComponent , title: 'Home Page'},
+    {path: 'about-us', component:AboutpageComponent, title: 'About Us'},
+    {path: 'contact-me', component:ContactComponent , title: 'Contact Me'}
+];
