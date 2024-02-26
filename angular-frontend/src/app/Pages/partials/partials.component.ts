c267ec415e89ecfc990b78d8941cabeb443b9013
Dianga Michael <diangamichael@gmail.com>
2024-02-26 21:00:50 +0300

    Created NavBar
    Created Partials and Navbar

---
+++ angular-frontend/src/app/Pages/partials/partials.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-partials',
+  standalone: true,
+  imports: [],
+  templateUrl: './partials.component.html',
+  styleUrl: './partials.component.css'
+})
+export class PartialsComponent {
+
+}
